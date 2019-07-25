95674a21d515185dd15e30137fc478e8570f2bf1
Sam Marks <sam@sammarks.me>
2019-07-25 08:03:39 -0400

    initial commit

---
+++ src/workflow.ts
@@ -0,0 +1,66 @@
+export type StepContext = object
+export type StepFunction = (context: StepContext) => Promise<void>
+
+export interface Step {
+  name: string
+  run: StepFunction
+  revert?: StepFunction
+}
+
+export interface WorkflowErrorInterface extends Error {
+  originalException: Error
+  stepName: string
+}
+
+export interface WorkflowRevertErrorInterface extends WorkflowErrorInterface {
+  originalWorkflowError: WorkflowError
+}
+
+export class WorkflowError extends Error implements WorkflowErrorInterface {
+  originalException: Error
+  stepName: string
+
+  constructor (stepName: string, originalException: Error) {
+    super(`Workflow error executing '${stepName}': ${originalException}`) /* istanbul ignore next */
+    this.stepName = stepName
+    this.originalException = originalException
+  }
+}
+
+export class WorkflowRevertError extends WorkflowError implements WorkflowRevertErrorInterface {
+  originalWorkflowError: WorkflowError
+
+  constructor (stepName: string, error: Error, originalWorkflowError: WorkflowError) {
+    super(stepName, error) /* istanbul ignore next */
+    this.originalWorkflowError = originalWorkflowError
+    this.message = `Workflow error executing '${originalWorkflowError.stepName}': ${originalWorkflowError.originalException}\nAdditionally, error reverting step '${stepName}': ${error}`
+  }
+}
+
+export const execute = async <T extends StepContext>(name: string, steps: Array<Step>, context: T): Promise<T> => {
+  const revertSteps: Array<Step> = []
+  console.info(`Starting workflow: '${name}'`)
+  for (let i = 0; i < steps.length; i++) {
+    const step = steps[i]
+    if (step.revert) revertSteps.push({ name: step.name, run: step.revert })
+    try {
+      console.info(`- Step: ${step.name}`)
+      await step.run(context)
+    } catch (e) {
+      console.error(` Failed.`)
+      console.error(e)
+      const workflowError = new WorkflowError(step.name, e)
+      try {
+        await execute(`${name} (revert)`, revertSteps, context)
+      } catch (err) {
+        throw new WorkflowRevertError(err.stepName, err.originalException, workflowError)
+      }
+      throw workflowError
+    }
+  }
+
+  console.info('Complete')
+  return context
+}
+
+export default execute
---
+++ test/workflow.test.ts
@@ -0,0 +1,127 @@
+import { execute, StepContext, WorkflowError, WorkflowRevertError } from '../src/workflow'
+
+interface TestContext extends StepContext {
+  another?: string
+  fallback?: boolean
+  second?: boolean
+}
+
+describe('#execute()', () => {
+  let firstStep: jest.Mock<Promise<void>, [TestContext]>
+  let firstStepFallback: jest.Mock<Promise<void>, [TestContext]>
+  let secondStep: jest.Mock<Promise<void>, [TestContext]>
+  let failStep: jest.Mock<Promise<void>, [TestContext]>
+  let failStepRevertNoError: jest.Mock<Promise<void>, [TestContext]>
+  let failStepRevert: jest.Mock<Promise<void>, [TestContext]>
+  let thirdStep: jest.Mock<Promise<void>, [TestContext]>
+  let thirdStepFallback: jest.Mock<Promise<void>, [TestContext]>
+  let boom: Error
+  let boomBoom: Error
+  beforeEach(() => {
+    boom = new Error('boom')
+    boomBoom = new Error('boom boom')
+    firstStep = jest.fn(async (context) => {
+      expect(context).toStrictEqual({ another: 'bar' })
+    })
+    firstStepFallback = jest.fn(async (context) => {
+      context.fallback = true
+    })
+    secondStep = jest.fn(async (context) => {
+      expect(context).toStrictEqual({ another: 'bar' })
+      context.second = true
+    })
+    failStep = jest.fn(async (context) => {
+      throw boom
+    })
+    failStepRevertNoError = jest.fn(async (context) => undefined)
+    failStepRevert = jest.fn(async (context) => {
+      throw boomBoom
+    })
+    thirdStep = jest.fn(async (context) => {
+      expect(context).toStrictEqual({ another: 'bar', second: true })
+    })
+    thirdStepFallback = jest.fn(async (context) => undefined)
+  })
+  describe('when all steps succeed', () => {
+    let result: TestContext
+    beforeEach(async () => {
+      result = await execute<TestContext>('test-workflow', [
+        { name: 'first', run: firstStep, revert: firstStepFallback },
+        { name: 'second', run: secondStep },
+        { name: 'third', run: thirdStep, revert: thirdStepFallback }
+      ], { another: 'bar' })
+    })
+    it('calls each step with the proper context', () => {
+      expect(firstStep.mock.calls).toStrictEqual([[result]])
+      expect(firstStepFallback.mock.calls).toStrictEqual([])
+      expect(secondStep.mock.calls).toStrictEqual([[result]])
+      expect(thirdStep.mock.calls).toStrictEqual([[result]])
+      expect(thirdStepFallback.mock.calls).toStrictEqual([])
+    })
+    it('returns the resulting context', () => {
+      expect(result).toStrictEqual({
+        another: 'bar',
+        second: true
+      })
+    })
+  })
+  describe('when one step fails', () => {
+    beforeEach(async () => {
+      try {
+        await execute<TestContext>('test-workflow', [
+          { name: 'first', run: firstStep, revert: firstStepFallback },
+          { name: 'second', run: secondStep },
+          { name: 'fail', run: failStep, revert: failStepRevertNoError },
+          { name: 'third', run: thirdStep, revert: thirdStepFallback }
+        ], { another: 'bar' })
+      } catch (err) {
+        expect(err.message).toEqual("Workflow error executing 'fail': Error: boom")
+        expect(err.originalException).toEqual(boom)
+        expect(err.stepName).toEqual('fail')
+      }
+    })
+    it('calls the revert steps', () => {
+      expect(failStepRevertNoError.mock.calls).toStrictEqual([[{ another: 'bar', second: true, fallback: true }]])
+      expect(firstStepFallback.mock.calls).toStrictEqual([[{ another: 'bar', second: true, fallback: true }]])
+    })
+  })
+  describe('when a revert step fails', () => {
+    it('throws the original error with the revertFailure error', async () => {
+      try {
+        await execute<TestContext>('test-workflow', [
+          { name: 'first', run: firstStep, revert: firstStepFallback },
+          { name: 'second', run: secondStep },
+          { name: 'fail', run: failStep, revert: failStepRevert },
+          { name: 'third', run: thirdStep, revert: thirdStepFallback }
+        ], { another: 'bar' })
+      } catch (err) {
+        expect(err.message).toEqual("Workflow error executing 'fail': Error: boom\nAdditionally, error reverting step 'fail': Error: boom boom")
+        expect(err.originalException).toEqual(boomBoom)
+        expect(err.originalWorkflowError.originalException).toEqual(boom)
+        expect(err.originalWorkflowError.stepName).toEqual('fail')
+        expect(err.stepName).toEqual('fail')
+      }
+    })
+  })
+})
+
+describe('WorkflowError', () => {
+  it('sets the appropriate properties', () => {
+    const exception = new Error('boom')
+    const instance = new WorkflowError('fail', exception)
+    expect(instance.message).toEqual("Workflow error executing 'fail': Error: boom")
+    expect(instance.originalException).toEqual(exception)
+    expect(instance.stepName).toEqual('fail')
+  })
+})
+
+describe('WorkflowRevertError', () => {
+  it('sets the appropriate properties', () => {
+    const exception = new Error('boom boom')
+    const originalException = new Error('boom')
+    const originalWorkflowError = new WorkflowError('fail', originalException)
+    const instance = new WorkflowRevertError('fail', exception, originalWorkflowError)
+    expect(instance.message).toEqual("Workflow error executing 'fail': Error: boom\nAdditionally, error reverting step 'fail': Error: boom boom")
+    expect(instance.originalWorkflowError).toEqual(originalWorkflowError)
+  })
+})
